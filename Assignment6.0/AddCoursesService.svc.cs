ec0ddfd0eed9656dc2a4b4a2d7b6a911f30acd8a
mayan <mayan@MAYANKS>
2021-10-23 12:43:03 -0700

    Add project files.

---
+++ Assignment6.0/AddCoursesService.svc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace Assignment6._0
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AddCoursesService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select AddCoursesService.svc or AddCoursesService.svc.cs at the Solution Explorer and start debugging.
+    public class AddCoursesService: IAddCoursesService
+    {
+        public class Course
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public Int32 seats { get; set; }
+        }
+
+        public class CourseRootObject
+        {
+            public Course[] courses { get; set; }
+        }
+
+        public Boolean addCourse(string Code, string Name, Int32 seats)
+        {
+            Course newCourse = new Course();
+            CourseRootObject courseObj = new CourseRootObject();
+            List<Course> coursesList = new List<Course>();
+            string json;
+            Boolean exists = false;
+            Boolean created = false;
+
+            string path = HttpRuntime.AppDomainAppPath + "\\courses_list.json";
+
+            string jsonData = File.ReadAllText(path);
+
+            courseObj = JsonConvert.DeserializeObject<CourseRootObject>(jsonData);
+
+            if(courseObj.courses != null)
+            {
+                coursesList = courseObj.courses.ToList<Course>();
+                foreach (Course course in coursesList)
+                {
+                    if(course.Code == Code)
+                    {
+                        exists = true;
+                    }
+                }
+            }
+
+            if (!exists)
+            {
+                newCourse.Code = Code; newCourse.Name = Name; newCourse.seats = seats;
+                coursesList.Add(newCourse);
+
+                courseObj.courses = coursesList.ToArray<Course>();
+                json = JsonConvert.SerializeObject(courseObj, Formatting.Indented);
+                File.WriteAllText(path, json);
+
+                created = true;
+            }
+            return created;
+        }
+    }
+}
---
+++ Assignment6.0/RegisterCourseService.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace Assignment6._0
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RegisterCourseService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RegisterCourseService.svc or RegisterCourseService.svc.cs at the Solution Explorer and start debugging.
+    public class RegisterCourseService : IRegisterCourseService
+    {
+        public class Course
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public Int32 seats { get; set; }
+        }
+
+        public class CourseRootObject
+        {
+            public Course[] courses { get; set; }
+        }
+        public string Registercourse(string courseCode, string userName)
+        {
+            List<Course> coursesList = new List<Course>();
+            CourseRootObject courseObj = new CourseRootObject();
+            string path = HttpRuntime.AppDomainAppPath + "\\courses_list.json";
+            string jsonData = File.ReadAllText(path);
+            string json;
+
+            courseObj = JsonConvert.DeserializeObject<CourseRootObject>(jsonData);
+            coursesList = courseObj.courses.ToList<Course>();
+            foreach (Course course in coursesList)
+            {
+                if (course.Code == courseCode)
+                {
+                    course.seats = course.seats - 1;
+                    courseObj.courses = coursesList.ToArray<Course>();
+                    json = JsonConvert.SerializeObject(courseObj, Formatting.Indented);
+                    File.WriteAllText(path, json);
+                }
+            }
+            string ans = string.Format("Course {0} has been registered for user {1}",courseCode,userName);
+            return ans;
+        }
+    }
+}
