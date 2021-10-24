c20d4c826ca913b0e4db33a28ec8dec2b5c559d5
Tanmay Jena <tjena2@gmail.com>
2021-10-23 17:37:13 -0700

    Added payment service, user list json

---
+++ Assignment6.0/PaymentService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Assignment6._0
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Payment" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Payment.svc or Payment.svc.cs at the Solution Explorer and start debugging.
+    public class Payment : IPaymentService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
