bef531504d34be0057521a23a7a0b2125ffb6eda
NAKUL VAIDYA <vaidyanakul15@gmail.com>
2021-10-23 17:58:34 -0700

    Added Register user service

---
+++ Assignment6.0/RegisterUserService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Assignment6._0
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RegisterUserService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RegisterUserService.svc or RegisterUserService.svc.cs at the Solution Explorer and start debugging.
+    public class RegisterUserService : IRegisterUserService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
